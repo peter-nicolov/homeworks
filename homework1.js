e86ef9f615b24a6608a6f618923163460a7f4414
peter-nicolov <105744490+peter-nicolov@users.noreply.github.com>
2024-05-25 23:17:10 +0300

    Add files via upload

---
+++ homework1.js
@@ -0,0 +1,60 @@
+    // Task 2
+
+console.log('Nicolov')
+
+    // Task 3
+
+let firstName = 'Peter';
+let lastName = 'Nicolov';
+console.log(firstName);
+console.log(lastName);
+
+firstName = 'Pepi';
+console.log(firstName);
+
+firstName = lastName;
+console.log(firstName);
+
+// Task 4
+
+ const myData = {
+    name: 'Peter Nicolov',
+    age: '35',
+    male: true,
+    isMarried: undefined,
+    hasKids: null
+ };
+console.log(myData);
+console.log(typeof myData);
+
+// Task 5
+
+let isAdult = confirm('Do you have the age for majority?');
+console.log(isAdult);
+
+// Task 7
+
+let userName = prompt('What is your name?');
+let userEmail = prompt('What is your email?');
+let userPass = prompt('What is your password?');
+
+console.log(`Dear ${userName}, your email is ${userEmail}, your password is ${userPass}.`)
+
+// Task 6
+
+let myName = 'Peter';
+let myLastName = 'Nicolov';
+let studyGroup = 3;
+let birthYear = 1988;
+let hasMarriage = false;
+let numberOfKids;
+let wifeName = null;
+
+console.log(typeof studyGroup);
+console.log(typeof birthYear);
+console.log(typeof hasMarriage);
+console.log(typeof myName);
+console.log(typeof myLastName);
+
+console.log(typeof numberOfKids);
+console.log(typeof wifeName);
